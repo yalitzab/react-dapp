efac2bf0c65c5171443a2973204f027cee55e06d
yalitzab <yalitzabueno@gmail.com>
2021-12-14 14:23:33 -0800

    add greeter address add asyn functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 function App() {
+  const [greeting, setGreetingValue] = useState ()
+
+  async function requestAccount() {
+
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  async function setGreeting() {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
